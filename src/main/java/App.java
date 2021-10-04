bee7613f4b1883b5c862cba04d111410c95350d0
shady-azouz <shady.azouz@gmail.com>
2021-10-04 14:23:08 +0200

    fix: change function name from converter to convert

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import org.apache.commons.codec.binary.Base64;
 
 public class App {
-    public byte[] converter(String s) {
+    public byte[] convert(String s) {
         return Base64.encodeBase64(s.getBytes());
     }
 }
