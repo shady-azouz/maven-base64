e64cedf917684219290aa3a65a07f6ff65d4a957
shady-azouz <shady.azouz@gmail.com>
2021-10-04 14:22:14 +0200

    mod: utilize commons.codec library

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
-import java.util.Base64;
+import org.apache.commons.codec.binary.Base64;
 
 public class App {
-    public String converter(String s) {
-        return Base64.getEncoder().encodeToString(s.getBytes());
+    public byte[] converter(String s) {
+        return Base64.encodeBase64(s.getBytes());
     }
 }
